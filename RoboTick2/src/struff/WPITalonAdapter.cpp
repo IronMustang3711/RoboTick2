cc9d1852430ca587865a54c2fea9ff0f9588a3e4
Jason Markham <jasonmarkham@gmail.com>
2018-01-31 14:34:51 -0800

    minimal wpi talon adapter. unused.

---
+++ RoboTick2/src/struff/WPITalonAdapter.cpp
@@ -0,0 +1,39 @@
+/*
+ * WPITalonAdapter.cpp
+ *
+ *  Created on: Jan 31, 2018
+ *      Author: jason
+ */
+
+#include "WPITalonAdapter.h"
+#include <ctre/Phoenix.h>
+WPITalonAdapter::WPITalonAdapter(Talon& talon): frc::SpeedController{}, delegate{talon}{
+}
+
+void WPITalonAdapter::PIDWrite(double output) {
+	Set(output);
+}
+
+void WPITalonAdapter::Set(double speed) {
+	delegate.Set(ControlMode::PercentOutput,speed);
+}
+
+double WPITalonAdapter::Get() const{
+	return delegate.GetMotorOutputPercent();
+}
+
+void WPITalonAdapter::SetInverted(bool isInverted) {
+	delegate.SetInverted(isInverted);
+}
+
+bool WPITalonAdapter::GetInverted() const {
+	return delegate.GetInverted();
+}
+
+void WPITalonAdapter::Disable() {
+	StopMotor();
+}
+
+void WPITalonAdapter::StopMotor() {
+	delegate.Set(ControlMode::PercentOutput,0);
+}
--- RoboTick2/src/struff/WPITalonAdapter.h
+++ RoboTick2/src/struff/WPITalonAdapter.h
@@ -1,4 +1,36 @@
 #pragma once
-#include <WPILib.h>
+#include <SpeedController.h>
+
+namespace ctre {
+namespace phoenix {
+namespace motorcontrol {
+namespace can {
+class TalonSRX;
+}
+}
+}
+}
+
+class WPITalonAdapter: public frc::SpeedController {
+public:
+	using Talon = ctre::phoenix::motorcontrol::can::TalonSRX;
+
+	WPITalonAdapter(Talon& talon);
+
+	//PIDOutput
+	void PIDWrite(double output) override;
+
+	//SpeedController
+	void Set(double speed) override;
+	double Get() const override;
+	void SetInverted(bool isInverted) override;
+	bool GetInverted() const override;
+	void Disable() override;
+	void StopMotor() override;
+
+private:
+	Talon& delegate;
+
+};
 
 
