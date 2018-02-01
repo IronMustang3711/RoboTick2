7f8dd8607e306c2f5c2da84bf31a423f253baaeb
Jason Markham <jasonmarkham@gmail.com>
2018-02-01 11:24:41 -0800

    make MyTalon name-compatible with TalonSRX.
    implementation is left for the reader to provide.

--- RoboTick2/src/stuff/WPITalonAdapter.cpp
+++ RoboTick2/src/stuff/WPITalonAdapter.cpp
@@ -8,31 +8,370 @@
 #include "WPITalonAdapter.h"
 #include <ctre/Phoenix.h>
 
-
-MyTalon::MyTalon(TalonImpl * imp): impl{imp}
-{
+MyTalon::MyTalon(TalonImpl * imp) :
+		impl { imp } {
+	assert(impl != nullptr);
 }
 
-
-void MyTalon::Set(double speed)
-{
+void MyTalon::Set(double speed) {
 	impl->Set(ControlMode::PercentOutput, speed);
 }
 
-double MyTalon::Get() const
-{
+double MyTalon::Get() const {
 	return impl->GetMotorOutputPercent();
 }
 
-void MyTalon::SetInverted(bool isInverted)
-{
+void MyTalon::SetInverted(bool isInverted) {
 	impl->SetInverted(isInverted);
 }
-
-bool MyTalon::GetInverted() const
-{
+bool MyTalon::GetInverted() const {
 	return impl->GetInverted();
 }
 
+int MyTalon::GetDeviceID() {
+}
 
+void MyTalon::Set(ControlMode Mode, double value) {
+}
 
+void MyTalon::Set(ControlMode mode, double demand0, double demand1) {
+}
+
+void MyTalon::NeutralOutput() {
+}
+
+void MyTalon::SetNeutralMode(NeutralMode neutralMode) {
+}
+
+void MyTalon::EnableHeadingHold(bool enable) {
+}
+
+void MyTalon::SelectDemandType(bool value) {
+}
+
+void MyTalon::SetSensorPhase(bool PhaseSensor) {
+}
+
+ctre::phoenix::ErrorCode MyTalon::ConfigOpenloopRamp(
+		double secondsFromNeutralToFull, int timeoutMs) {
+}
+
+ctre::phoenix::ErrorCode MyTalon::ConfigClosedloopRamp(
+		double secondsFromNeutralToFull, int timeoutMs) {
+}
+
+ctre::phoenix::ErrorCode MyTalon::ConfigPeakOutputForward(double percentOut,
+		int timeoutMs) {
+}
+
+ctre::phoenix::ErrorCode MyTalon::ConfigPeakOutputReverse(double percentOut,
+		int timeoutMs) {
+}
+
+ctre::phoenix::ErrorCode MyTalon::ConfigNominalOutputForward(double percentOut,
+		int timeoutMs) {
+}
+
+ctre::phoenix::ErrorCode MyTalon::ConfigNominalOutputReverse(double percentOut,
+		int timeoutMs) {
+}
+
+ctre::phoenix::ErrorCode MyTalon::ConfigNeutralDeadband(double percentDeadband,
+		int timeoutMs) {
+}
+
+ctre::phoenix::ErrorCode MyTalon::ConfigVoltageCompSaturation(double voltage,
+		int timeoutMs) {
+}
+
+ctre::phoenix::ErrorCode MyTalon::ConfigVoltageMeasurementFilter(
+		int filterWindowSamples, int timeoutMs) {
+}
+
+void MyTalon::EnableVoltageCompensation(bool enable) {
+}
+
+double MyTalon::GetBusVoltage() {
+}
+
+double MyTalon::GetMotorOutputPercent() {
+}
+
+double MyTalon::GetMotorOutputVoltage() {
+}
+
+double MyTalon::GetOutputCurrent() {
+}
+
+double MyTalon::GetTemperature() {
+}
+
+ctre::phoenix::ErrorCode MyTalon::ConfigSelectedFeedbackSensor(
+		RemoteFeedbackDevice feedbackDevice, int pidIdx, int timeoutMs) {
+}
+
+ctre::phoenix::ErrorCode MyTalon::ConfigSelectedFeedbackSensor(
+		FeedbackDevice feedbackDevice, int pidIdx, int timeoutMs) {
+}
+
+ctre::phoenix::ErrorCode MyTalon::ConfigRemoteFeedbackFilter(int deviceID,
+		RemoteSensorSource remoteSensorSource, int remoteOrdinal,
+		int timeoutMs) {
+}
+
+ctre::phoenix::ErrorCode MyTalon::ConfigSensorTerm(SensorTerm sensorTerm,
+		FeedbackDevice feedbackDevice, int timeoutMs) {
+}
+
+int MyTalon::GetSelectedSensorPosition(int pidIdx) {
+}
+
+int MyTalon::GetSelectedSensorVelocity(int pidIdx) {
+}
+
+ctre::phoenix::ErrorCode MyTalon::SetSelectedSensorPosition(int sensorPos,
+		int pidIdx, int timeoutMs) {
+}
+
+ctre::phoenix::ErrorCode MyTalon::SetControlFramePeriod(ControlFrame frame,
+		int periodMs) {
+}
+
+ctre::phoenix::ErrorCode MyTalon::SetStatusFramePeriod(StatusFrame frame,
+		int periodMs, int timeoutMs) {
+}
+
+ctre::phoenix::ErrorCode MyTalon::SetStatusFramePeriod(
+		StatusFrameEnhanced frame, int periodMs, int timeoutMs) {
+}
+
+int MyTalon::GetStatusFramePeriod(StatusFrame frame, int timeoutMs) {
+}
+
+int MyTalon::GetStatusFramePeriod(StatusFrameEnhanced frame, int timeoutMs) {
+}
+
+ctre::phoenix::ErrorCode MyTalon::ConfigVelocityMeasurementPeriod(
+		VelocityMeasPeriod period, int timeoutMs) {
+}
+
+ctre::phoenix::ErrorCode MyTalon::ConfigForwardLimitSwitchSource(
+		RemoteLimitSwitchSource type, LimitSwitchNormal normalOpenOrClose,
+		int deviceID, int timeoutMs) {
+}
+
+ctre::phoenix::ErrorCode MyTalon::ConfigReverseLimitSwitchSource(
+		RemoteLimitSwitchSource type, LimitSwitchNormal normalOpenOrClose,
+		int deviceID, int timeoutMs) {
+}
+
+void MyTalon::OverrideLimitSwitchesEnable(bool enable) {
+}
+
+ctre::phoenix::ErrorCode MyTalon::ConfigForwardLimitSwitchSource(
+		LimitSwitchSource type, LimitSwitchNormal normalOpenOrClose,
+		int timeoutMs) {
+}
+
+ctre::phoenix::ErrorCode MyTalon::ConfigReverseLimitSwitchSource(
+		LimitSwitchSource type, LimitSwitchNormal normalOpenOrClose,
+		int timeoutMs) {
+}
+
+ctre::phoenix::ErrorCode MyTalon::ConfigForwardSoftLimitThreshold(
+		int forwardSensorLimit, int timeoutMs) {
+}
+
+ctre::phoenix::ErrorCode MyTalon::ConfigReverseSoftLimitThreshold(
+		int reverseSensorLimit, int timeoutMs) {
+}
+
+ctre::phoenix::ErrorCode MyTalon::ConfigForwardSoftLimitEnable(bool enable,
+		int timeoutMs) {
+}
+
+ctre::phoenix::ErrorCode MyTalon::ConfigReverseSoftLimitEnable(bool enable,
+		int timeoutMs) {
+}
+
+void MyTalon::OverrideSoftLimitsEnable(bool enable) {
+}
+
+ctre::phoenix::ErrorCode MyTalon::ConfigVelocityMeasurementWindow(
+		int windowSize, int timeoutMs) {
+}
+
+ctre::phoenix::ErrorCode MyTalon::Config_kP(int slotIdx, double value,
+		int timeoutMs) {
+}
+
+ctre::phoenix::ErrorCode MyTalon::Config_kI(int slotIdx, double value,
+		int timeoutMs) {
+}
+
+ctre::phoenix::ErrorCode MyTalon::Config_kD(int slotIdx, double value,
+		int timeoutMs) {
+}
+
+ctre::phoenix::ErrorCode MyTalon::Config_kF(int slotIdx, double value,
+		int timeoutMs) {
+}
+
+ctre::phoenix::ErrorCode MyTalon::Config_IntegralZone(int slotIdx, int izone,
+		int timeoutMs) {
+}
+
+ctre::phoenix::ErrorCode MyTalon::ConfigAllowableClosedloopError(int slotIdx,
+		int allowableCloseLoopError, int timeoutMs) {
+}
+
+ctre::phoenix::ErrorCode MyTalon::ConfigMaxIntegralAccumulator(int slotIdx,
+		double iaccum, int timeoutMs) {
+}
+
+ctre::phoenix::ErrorCode MyTalon::SetIntegralAccumulator(double iaccum,
+		int pidIdx, int timeoutMs) {
+}
+
+int MyTalon::GetClosedLoopError(int pidIdx) {
+}
+
+double MyTalon::GetIntegralAccumulator(int pidIdx) {
+}
+
+double MyTalon::GetErrorDerivative(int pidIdx) {
+}
+
+ctre::phoenix::ErrorCode MyTalon::SelectProfileSlot(int slotIdx, int pidIdx) {
+}
+
+int MyTalon::GetClosedLoopTarget(int pidIdx) {
+}
+
+int MyTalon::GetActiveTrajectoryPosition() {
+}
+
+int MyTalon::GetActiveTrajectoryVelocity() {
+}
+
+double MyTalon::GetActiveTrajectoryHeading() {
+}
+
+ctre::phoenix::ErrorCode MyTalon::ConfigMotionCruiseVelocity(
+		int sensorUnitsPer100ms, int timeoutMs) {
+}
+
+ctre::phoenix::ErrorCode MyTalon::ConfigMotionAcceleration(
+		int sensorUnitsPer100msPerSec, int timeoutMs) {
+}
+
+void MyTalon::ClearMotionProfileTrajectories() {
+}
+
+int MyTalon::GetMotionProfileTopLevelBufferCount() {
+}
+
+ctre::phoenix::ErrorCode MyTalon::PushMotionProfileTrajectory(
+		const ctre::phoenix::motion::TrajectoryPoint& trajPt) {
+}
+
+bool MyTalon::IsMotionProfileTopLevelBufferFull() {
+}
+
+void MyTalon::ProcessMotionProfileBuffer() {
+}
+
+ctre::phoenix::ErrorCode MyTalon::GetMotionProfileStatus(
+		ctre::phoenix::motion::MotionProfileStatus& statusToFill) {
+}
+
+ctre::phoenix::ErrorCode MyTalon::ClearMotionProfileHasUnderrun(int timeoutMs) {
+}
+
+ctre::phoenix::ErrorCode MyTalon::ChangeMotionControlFramePeriod(int periodMs) {
+}
+
+ctre::phoenix::ErrorCode MyTalon::ConfigMotionProfileTrajectoryPeriod(
+		int baseTrajDurationMs, int timeoutMs) {
+}
+
+ctre::phoenix::ErrorCode MyTalon::GetLastError() {
+}
+
+ctre::phoenix::ErrorCode MyTalon::GetFaults(Faults& toFill) {
+}
+
+ctre::phoenix::ErrorCode MyTalon::GetStickyFaults(StickyFaults& toFill) {
+}
+
+ctre::phoenix::ErrorCode MyTalon::ClearStickyFaults(int timeoutMs) {
+}
+
+int MyTalon::GetFirmwareVersion() {
+}
+
+bool MyTalon::HasResetOccurred() {
+}
+
+ctre::phoenix::ErrorCode MyTalon::ConfigSetCustomParam(int newValue,
+		int paramIndex, int timeoutMs) {
+}
+
+int MyTalon::ConfigGetCustomParam(int paramIndex, int timeoutMs) {
+}
+
+ctre::phoenix::ErrorCode MyTalon::ConfigSetParameter(
+		ctre::phoenix::ParamEnum param, double value, uint8_t subValue,
+		int ordinal, int timeoutMs) {
+}
+
+double MyTalon::ConfigGetParameter(ctre::phoenix::ParamEnum param, int ordinal,
+		int timeoutMs) {
+}
+
+int MyTalon::GetBaseID() {
+}
+
+ControlMode MyTalon::GetControlMode() {
+}
+
+void MyTalon::ValueUpdated() {
+}
+
+ctre::phoenix::motorcontrol::SensorCollection& MyTalon::GetSensorCollection() {
+}
+
+ctre::phoenix::ErrorCode MyTalon::ConfigForwardLimitSwitchSource(
+		LimitSwitchSource limitSwitchSource,
+		LimitSwitchNormal normalOpenOrClose, int timeoutMs) {
+}
+
+ctre::phoenix::ErrorCode MyTalon::ConfigReverseLimitSwitchSource(
+		LimitSwitchSource limitSwitchSource,
+		LimitSwitchNormal normalOpenOrClose, int timeoutMs) {
+}
+
+ctre::phoenix::ErrorCode MyTalon::ConfigForwardLimitSwitchSource(
+		RemoteLimitSwitchSource limitSwitchSource,
+		LimitSwitchNormal normalOpenOrClose, int deviceID, int timeoutMs) {
+}
+
+ctre::phoenix::ErrorCode MyTalon::ConfigReverseLimitSwitchSource(
+		RemoteLimitSwitchSource limitSwitchSource,
+		LimitSwitchNormal normalOpenOrClose, int deviceID, int timeoutMs) {
+}
+
+ctre::phoenix::ErrorCode MyTalon::ConfigPeakCurrentLimit(int amps,
+		int timeoutMs) {
+}
+
+ctre::phoenix::ErrorCode MyTalon::ConfigPeakCurrentDuration(int milliseconds,
+		int timeoutMs) {
+}
+
+ctre::phoenix::ErrorCode MyTalon::ConfigContinuousCurrentLimit(int amps,
+		int timeoutMs) {
+}
+
+void MyTalon::EnableCurrentLimit(bool enable) {
+}
--- RoboTick2/src/stuff/WPITalonAdapter.h
+++ RoboTick2/src/stuff/WPITalonAdapter.h
@@ -1,5 +1,21 @@
 #pragma once
 #include <SpeedController.h>
+#include "ctre/phoenix/ErrorCode.h"
+#include "ctre/phoenix/paramEnum.h"
+#include "ctre/phoenix/MotorControl/ControlMode.h"
+#include "ctre/phoenix/MotorControl/Faults.h"
+#include "ctre/phoenix/MotorControl/StickyFaults.h"
+#include "ctre/phoenix/MotorControl/FeedbackDevice.h"
+#include "ctre/phoenix/MotorControl/StatusFrame.h"
+#include "ctre/phoenix/MotorControl/NeutralMode.h"
+#include "ctre/phoenix/MotorControl/LimitSwitchType.h"
+#include "ctre/phoenix/MotorControl/RemoteSensorSource.h"
+#include "ctre/phoenix/MotorControl/ControlFrame.h"
+#include "ctre/phoenix/MotorControl/VelocityMeasPeriod.h"
+#include "ctre/phoenix/MotorControl/SensorTerm.h"
+#include "ctre/phoenix/Motion/TrajectoryPoint.h"
+#include "ctre/phoenix/Motion/MotionProfileStatus.h"
+#include "ctre/phoenix/MotorControl/SensorCollection.h"
 
 namespace ctre {
 namespace phoenix {
@@ -11,39 +27,252 @@ class TalonSRX;
 }
 }
 
+using namespace ctre::phoenix::motorcontrol;
 
-class MyTalon : public frc::SpeedController {
+class MyTalon: public frc::SpeedController {
 
 	using TalonImpl = ctre::phoenix::motorcontrol::can::TalonSRX;
-
 public:
 	MyTalon(TalonImpl* impl);
 
 	MyTalon(const MyTalon& other) = default;
-	MyTalon(MyTalon&& other)      = default;
+	MyTalon(MyTalon&& other) = default;
 
-	MyTalon& operator= (const MyTalon& other) = default;
-	MyTalon& operator= (MyTalon&& other)      = default;
-
-
-
-
-
+	MyTalon& operator=(const MyTalon& other) = default;
+	MyTalon& operator=(MyTalon&& other) = default;
 
 	//PIDOutput
-	inline void PIDWrite(double output) override { Set(output); }
+	inline void PIDWrite(double output) override {
+		Set(output);
+	}
 
 	//SpeedController
 	void Set(double speed) override;
 	double Get() const override;
 	void SetInverted(bool isInverted) override;
 	bool GetInverted() const override;
-	inline void Disable() override { StopMotor(); }
-	inline void StopMotor() override { Set(0); }
+	inline void Disable() override {
+		StopMotor();
+	}
+	inline void StopMotor() override {
+		Set(0);
+	}
 
+	//BaseMotorController (ctre)
+
+	int GetDeviceID();
+	void Set(ControlMode Mode, double value);
+	void Set(ControlMode mode, double demand0, double demand1);
+	void NeutralOutput();
+	void SetNeutralMode(NeutralMode neutralMode);
+	void EnableHeadingHold(bool enable);
+	void SelectDemandType(bool value);
+	//------ Invert behavior ----------//
+	void SetSensorPhase(bool PhaseSensor);
+	void SetInverted(bool invert);
+	bool GetInverted() const;
+	//----- general output shaping ------------------//
+	ctre::phoenix::ErrorCode ConfigOpenloopRamp(double secondsFromNeutralToFull,
+			int timeoutMs);
+	ctre::phoenix::ErrorCode ConfigClosedloopRamp(
+			double secondsFromNeutralToFull, int timeoutMs);
+	ctre::phoenix::ErrorCode ConfigPeakOutputForward(double percentOut,
+			int timeoutMs);
+	ctre::phoenix::ErrorCode ConfigPeakOutputReverse(double percentOut,
+			int timeoutMs);
+	ctre::phoenix::ErrorCode ConfigNominalOutputForward(double percentOut,
+			int timeoutMs);
+	ctre::phoenix::ErrorCode ConfigNominalOutputReverse(double percentOut,
+			int timeoutMs);
+	ctre::phoenix::ErrorCode ConfigNeutralDeadband(double percentDeadband,
+			int timeoutMs);
+	//------ Voltage Compensation ----------//
+	ctre::phoenix::ErrorCode ConfigVoltageCompSaturation(double voltage,
+			int timeoutMs);
+	ctre::phoenix::ErrorCode ConfigVoltageMeasurementFilter(
+			int filterWindowSamples, int timeoutMs);
+	void EnableVoltageCompensation(bool enable);
+	//------ General Status ----------//
+	double GetBusVoltage();
+	double GetMotorOutputPercent();
+	double GetMotorOutputVoltage();
+	double GetOutputCurrent();
+	double GetTemperature();
+	//------ sensor selection ----------//
+	ctre::phoenix::ErrorCode ConfigSelectedFeedbackSensor(
+			RemoteFeedbackDevice feedbackDevice, int pidIdx, int timeoutMs);
+	ctre::phoenix::ErrorCode ConfigSelectedFeedbackSensor(
+			FeedbackDevice feedbackDevice, int pidIdx, int timeoutMs);
+	ctre::phoenix::ErrorCode ConfigRemoteFeedbackFilter(int deviceID,
+			RemoteSensorSource remoteSensorSource, int remoteOrdinal,
+			int timeoutMs);
+	ctre::phoenix::ErrorCode ConfigSensorTerm(SensorTerm sensorTerm,
+			FeedbackDevice feedbackDevice, int timeoutMs);
+
+	//------- sensor status --------- //
+	int GetSelectedSensorPosition(int pidIdx);
+	int GetSelectedSensorVelocity(int pidIdx);
+	ctre::phoenix::ErrorCode SetSelectedSensorPosition(int sensorPos,
+			int pidIdx, int timeoutMs);
+	//------ status frame period changes ----------//
+	ctre::phoenix::ErrorCode SetControlFramePeriod(ControlFrame frame,
+			int periodMs);
+	ctre::phoenix::ErrorCode SetStatusFramePeriod(StatusFrame frame,
+			int periodMs, int timeoutMs);
+	ctre::phoenix::ErrorCode SetStatusFramePeriod(StatusFrameEnhanced frame,
+			int periodMs, int timeoutMs);
+	int GetStatusFramePeriod(StatusFrame frame, int timeoutMs);
+	int GetStatusFramePeriod(StatusFrameEnhanced frame, int timeoutMs);
+	//----- velocity signal conditionaing ------//
+	ctre::phoenix::ErrorCode ConfigVelocityMeasurementPeriod(
+			VelocityMeasPeriod period, int timeoutMs);
+	ctre::phoenix::ErrorCode ConfigVelocityMeasurementWindow(int windowSize,
+			int timeoutMs);
+	//------ remote limit switch ----------//
+	 ctre::phoenix::ErrorCode ConfigForwardLimitSwitchSource(
+		RemoteLimitSwitchSource type, LimitSwitchNormal normalOpenOrClose,
+		int deviceID, int timeoutMs);
+	 ctre::phoenix::ErrorCode ConfigReverseLimitSwitchSource(
+		RemoteLimitSwitchSource type, LimitSwitchNormal normalOpenOrClose,
+		int deviceID, int timeoutMs);
+	void OverrideLimitSwitchesEnable(bool enable);
+	////------ local limit switch ----------//
+	 ctre::phoenix::ErrorCode ConfigForwardLimitSwitchSource(LimitSwitchSource type,
+		LimitSwitchNormal normalOpenOrClose, int timeoutMs);
+	 ctre::phoenix::ErrorCode ConfigReverseLimitSwitchSource(LimitSwitchSource type,
+		LimitSwitchNormal normalOpenOrClose, int timeoutMs);
+	////------ soft limit ----------//
+	 ctre::phoenix::ErrorCode ConfigForwardSoftLimitThreshold(int forwardSensorLimit,
+		int timeoutMs);
+	 ctre::phoenix::ErrorCode ConfigReverseSoftLimitThreshold(int reverseSensorLimit,
+		int timeoutMs);
+	 ctre::phoenix::ErrorCode ConfigForwardSoftLimitEnable(bool enable,
+		int timeoutMs);
+	 ctre::phoenix::ErrorCode ConfigReverseSoftLimitEnable(bool enable,
+		int timeoutMs);
+	 void OverrideSoftLimitsEnable(bool enable);
+	//------ Current Lim ----------//
+	/* not available in base */
+	//------ General Close loop ----------//
+	ctre::phoenix::ErrorCode Config_kP(int slotIdx, double value,
+			int timeoutMs);
+	ctre::phoenix::ErrorCode Config_kI(int slotIdx, double value,
+			int timeoutMs);
+	ctre::phoenix::ErrorCode Config_kD(int slotIdx, double value,
+			int timeoutMs);
+	ctre::phoenix::ErrorCode Config_kF(int slotIdx, double value,
+			int timeoutMs);
+	ctre::phoenix::ErrorCode Config_IntegralZone(int slotIdx, int izone,
+			int timeoutMs);
+	ctre::phoenix::ErrorCode ConfigAllowableClosedloopError(int slotIdx,
+			int allowableCloseLoopError, int timeoutMs);
+	ctre::phoenix::ErrorCode ConfigMaxIntegralAccumulator(int slotIdx,
+			double iaccum, int timeoutMs);
+	//------ Close loop State ----------//
+	ctre::phoenix::ErrorCode SetIntegralAccumulator(double iaccum, int pidIdx,
+			int timeoutMs);
+	int GetClosedLoopError(int pidIdx);
+	double GetIntegralAccumulator(int pidIdx);
+	double GetErrorDerivative(int pidIdx);
+
+	ctre::phoenix::ErrorCode SelectProfileSlot(int slotIdx, int pidIdx);
+
+	int GetClosedLoopTarget(int pidIdx);
+	int GetActiveTrajectoryPosition();
+	int GetActiveTrajectoryVelocity();
+	double GetActiveTrajectoryHeading();
+
+	//------ Motion Profile Settings used in Motion Magic  ----------//
+	ctre::phoenix::ErrorCode ConfigMotionCruiseVelocity(int sensorUnitsPer100ms,
+			int timeoutMs);
+	ctre::phoenix::ErrorCode ConfigMotionAcceleration(
+			int sensorUnitsPer100msPerSec, int timeoutMs);
+	//------ Motion Profile Buffer ----------//
+	void ClearMotionProfileTrajectories();
+	int GetMotionProfileTopLevelBufferCount();
+	ctre::phoenix::ErrorCode PushMotionProfileTrajectory(
+			const ctre::phoenix::motion::TrajectoryPoint & trajPt);
+	bool IsMotionProfileTopLevelBufferFull();
+	void ProcessMotionProfileBuffer();
+	ctre::phoenix::ErrorCode GetMotionProfileStatus(
+			ctre::phoenix::motion::MotionProfileStatus & statusToFill);
+	ctre::phoenix::ErrorCode ClearMotionProfileHasUnderrun(int timeoutMs);
+	ctre::phoenix::ErrorCode ChangeMotionControlFramePeriod(int periodMs);
+	ctre::phoenix::ErrorCode ConfigMotionProfileTrajectoryPeriod(
+			int baseTrajDurationMs, int timeoutMs);
+	//------ error ----------//
+	ctre::phoenix::ErrorCode GetLastError();
+	//------ Faults ----------//
+	ctre::phoenix::ErrorCode GetFaults(Faults & toFill);
+	ctre::phoenix::ErrorCode GetStickyFaults(StickyFaults & toFill);
+	ctre::phoenix::ErrorCode ClearStickyFaults(int timeoutMs);
+	//------ Firmware ----------//
+	int GetFirmwareVersion();
+	bool HasResetOccurred();
+	//------ Custom Persistent Params ----------//
+	ctre::phoenix::ErrorCode ConfigSetCustomParam(int newValue, int paramIndex,
+			int timeoutMs);
+	int ConfigGetCustomParam(int paramIndex, int timeoutMs);
+	//------ Generic Param API, typically not used ----------//
+	ctre::phoenix::ErrorCode ConfigSetParameter(ctre::phoenix::ParamEnum param,
+			double value, uint8_t subValue, int ordinal, int timeoutMs);
+	double ConfigGetParameter(ctre::phoenix::ParamEnum param, int ordinal,
+			int timeoutMs);
+	//------ Misc. ----------//
+	int GetBaseID();
+	ControlMode GetControlMode();
+	// ----- Follower ------//
+	// void Follow(IMotorController & masterToFollow); //TODO!!
+	void ValueUpdated();
+
+	//------ RAW Sensor API ----------//
+	/**
+	 * @retrieve object that can get/set individual RAW sensor values.
+	 */
+	ctre::phoenix::motorcontrol::SensorCollection & GetSensorCollection();
+
+	ctre::phoenix::ErrorCode ConfigSelectedFeedbackSensor(
+			FeedbackDevice feedbackDevice, int pidIdx, int timeoutMs);
+	ctre::phoenix::ErrorCode ConfigSelectedFeedbackSensor(
+			RemoteFeedbackDevice feedbackDevice, int pidIdx, int timeoutMs);
+
+	ctre::phoenix::ErrorCode SetStatusFramePeriod(StatusFrameEnhanced frame,
+			int periodMs, int timeoutMs);
+	ctre::phoenix::ErrorCode SetStatusFramePeriod(StatusFrame frame,
+			int periodMs, int timeoutMs);
+
+	int GetStatusFramePeriod(StatusFrameEnhanced frame, int timeoutMs);
+	int GetStatusFramePeriod(StatusFrame frame, int timeoutMs);
+
+	//------ Velocity measurement ----------//
+	ctre::phoenix::ErrorCode ConfigVelocityMeasurementPeriod(
+			VelocityMeasPeriod period, int timeoutMs);
+	ctre::phoenix::ErrorCode ConfigVelocityMeasurementWindow(int windowSize,
+			int timeoutMs);
+
+	//------ limit switch ----------//
+	ctre::phoenix::ErrorCode ConfigForwardLimitSwitchSource(
+			LimitSwitchSource limitSwitchSource,
+			LimitSwitchNormal normalOpenOrClose, int timeoutMs);
+	ctre::phoenix::ErrorCode ConfigReverseLimitSwitchSource(
+			LimitSwitchSource limitSwitchSource,
+			LimitSwitchNormal normalOpenOrClose, int timeoutMs);
+	ctre::phoenix::ErrorCode ConfigForwardLimitSwitchSource(
+			RemoteLimitSwitchSource limitSwitchSource,
+			LimitSwitchNormal normalOpenOrClose, int deviceID, int timeoutMs);
+	ctre::phoenix::ErrorCode ConfigReverseLimitSwitchSource(
+			RemoteLimitSwitchSource limitSwitchSource,
+			LimitSwitchNormal normalOpenOrClose, int deviceID, int timeoutMs);
+
+	//------ Current Limit ----------//
+	ctre::phoenix::ErrorCode ConfigPeakCurrentLimit(int amps, int timeoutMs);
+	ctre::phoenix::ErrorCode ConfigPeakCurrentDuration(int milliseconds,
+			int timeoutMs);
+	ctre::phoenix::ErrorCode ConfigContinuousCurrentLimit(int amps,
+			int timeoutMs);
+	void EnableCurrentLimit(bool enable);
 
 private:
 	TalonImpl* impl;
 };
-
 
